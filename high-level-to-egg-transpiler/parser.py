adab2b6624b77389c6388cf75844f9170d768819
Ant13731 <69334555+Ant13731@users.noreply.github.com>
2025-04-24 15:18:28 -0400

    finish rudimentary grammar -> egg converter

--- high-level-to-egg-transpiler/parser.py
+++ high-level-to-egg-transpiler/parser.py
@@ -21,12 +21,12 @@ def parse(input_string: str, i: int):
     # Create a Lark parser with the defined grammar
     with open("high-level-to-egg-transpiler/very_high_level_grammar.lark", "r") as f:
         grammar = f.read()
-        parser = lark.Lark(grammar, start="start", postlex=PythonIndenter())
+        parser = lark.Lark(grammar, start="start", postlex=PythonIndenter(), maybe_placeholders=True)
 
     # Parse the input string and return the result
     tree = parser.parse(input_string)
     print(tree.pretty())
-    lark.tree.pydot__tree_to_png(tree, f"high-level-to-egg-transpiler/generated_images/tree{i}.png")
+    # lark.tree.pydot__tree_to_png(tree, f"high-level-to-egg-transpiler/generated_images/tree{i}.png")
     ast = GrammarToEggTransformer().transform(tree)
     print(ast)
 
@@ -42,10 +42,99 @@ f: float = 3.14
 g: set = {1, 2, 3}
 h: tuple = (1, 2, 3)
 i: None = None
-j: list = [for i in c: i*i]
 z: bool = a in c
+j: list = [for i in c: i*i]
 k: dict = {for i in c: (i,i*i)}
 l: set = {for i in c: i*i}
+a => b
+m: bool = a == b => e
+a != b and b > a
+b < a and b >= a
+a is a or a is not b
+b <= a or a not in g
+not e != e
+not e is not (not e)
+a + (- (+ b)) - c * d / e % f // g
+a + b - c * d / e % f // g
+a()
+a(1, 2, 3)
+a[]
+a[:]
+a[::]
+a[1:]
+a[:1]
+a[::1]
+a[:1:1]
+a[1::1]
+a[1:2:3]
+j: list = [for i in c | i < 0 and i == 0: i*i]
+k: dict = {for i in c| i < 0: (i,i*i)}
+l: set = {for i in c| i < 0: i*i}
+i^2
+a.b.c
+lambda (a: int,b:int): a + b
+lambda (): a + b
+return a
+return
+break
+continue
 """
-test_strs = test_str.split("\n")
-[parse(t, i) for i, t in enumerate(test_strs)]
+# test_strs = test_str.split("\n")
+# for i, t in enumerate(test_strs[-4:]):
+#     print(f"Parsing string {i}: {t}")
+#     parse(t, i)
+test_compound_strs = [
+    """
+if a: b
+""",
+    """
+if a:
+
+    b
+""",
+    """
+if a:
+    if b:
+        c
+    else:
+        d
+elif a and b:
+    if c:
+        d
+else:
+    e
+""",
+    """
+for i in a: b
+""",
+    """
+for i in a:
+    for j in b:
+        c
+""",
+    """
+struct a:
+    a: int
+    b: str
+""",
+    """
+enum a:
+    a
+    b
+""",
+    """
+struct a:
+    pass
+""",
+    """
+enum a:
+    pass
+""",
+    """
+def a(b: int, c: str) -> int:
+    return b + c
+""",
+]
+for i, t in enumerate(test_compound_strs):
+    print(f"Parsing string {i}: {t}")
+    parse(t, i)
