ba810828d372c4971ad1e0436b0c765b71a7405a
Ant13731 <69334555+Ant13731@users.noreply.github.com>
2025-06-09 23:43:42 -0400

    add biome for formatting

--- packages/simile_jupyter_extension/src/index.ts
+++ packages/simile_jupyter_extension/src/index.ts
@@ -1,18 +1,18 @@
-import {
-  JupyterFrontEnd,
-  JupyterFrontEndPlugin
-} from '@jupyterlab/application';
+import type {
+	JupyterFrontEnd,
+	JupyterFrontEndPlugin,
+} from "@jupyterlab/application";
 
 /**
  * Initialization data for the simile_lexer extension.
  */
 const plugin: JupyterFrontEndPlugin<void> = {
-  id: 'simile_lexer:plugin',
-  description: 'CodeMirror lexer for Simile',
-  autoStart: true,
-  activate: (app: JupyterFrontEnd) => {
-    console.log('JupyterLab extension simile_lexer is activated!');
-  }
+	id: "simile_lexer:plugin",
+	description: "CodeMirror lexer for Simile",
+	autoStart: true,
+	activate: (app: JupyterFrontEnd) => {
+		console.log("JupyterLab extension simile_lexer is activated!");
+	},
 };
 
 export default plugin;
