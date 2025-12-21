8f9664354b62eaf0d2c6ed8357a7258cf09e0846
Ant13731 <69334555+Ant13731@users.noreply.github.com>
2025-12-20 22:40:09 -0500

    making interface for llvm impl of bitsets

--- packages/simile_compiler/src/mod/implementations/interfaces/base.py
+++ packages/simile_compiler/src/mod/implementations/interfaces/base.py
@@ -6,6 +6,22 @@ from typing import Callable, ClassVar, Type, TypeVar
 
 @dataclass
 class BaseImplementation:
-    """Base class for all Simile implementation libraries."""
+    """Base class for all Simile implementation libraries.
+    Every class represents a Simile object, along with implementations for initialization, cleanup, and operations.
+    """
 
     target: ClassVar[str] = "llvm"
+    _numerical_id_counter: ClassVar[int] = 0
+    _numerical_id: int = field(init=False)
+
+    def __post_init__(self) -> None:
+        self._numerical_id = BaseImplementation._numerical_id_counter
+        BaseImplementation._numerical_id_counter += 1
+
+    def create_object(self) -> str:
+        """All information needed to create the object should be passed through the class init.
+        This function just spits out the generated code to create the object."""
+        raise NotImplementedError
+
+    def cleanup_object(self) -> str:
+        raise NotImplementedError
